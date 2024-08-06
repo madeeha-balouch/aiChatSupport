b32b5db3e81a3327f806304cf3c67a3293735220
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-08-06 14:11:54 +0400

    gemini api commit

--- chatsupport/app/api/chat/route.js
+++ chatsupport/app/api/chat/route.js
@@ -1,5 +1,17 @@
 import { NextResponse } from "next/server"
-export function POST(req){
-    console.log('POST/api/chat')
-    return NextResponse.json({message: 'Hello from the server!'})
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+export async function POST(req){
+    const data = await req.json()
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+    const prompt = "What are you made of?"
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    return NextResponse.json({message: text})
 }
