90c433b8bef700305bb3d1148540dffdfb5c25da
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-08-07 15:11:30 +0400

    page.js update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { useState } from "react";
+import { Box } from "@mui/system";
 
 export default function Home() {
-  
+  const [messages, setMessages] = useState({
+    role: "assistant",
+    content: "Hi I'm you support agent today, how can I assist you today?",
+  });
+
+  const [message, setMessage] = useState("");
+
+  return <Box width="100vw" height={"100vh"} display={"flex"}></Box>;
 }
